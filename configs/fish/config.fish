193082c0812366448c7533b61358fc0b880537bd
Colby Braden <colby.braden@gmail.com>
2023-08-15 15:36:08 -0500

    upload configs and wallpaper

---
+++ configs/fish/config.fish
@@ -0,0 +1,4 @@
+alias v="nvim"
+alias reboot="systemctl reboot"
+alias shutdown="systemctl poweroff"
+alias backports="sudo apt -t bookworm-backports install"
---
+++ configs/qtile/autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+lxpolkit &
+picom &
+pipewire &
+pipewire-pulse &
+xfce4-power-manager &
+nitrogen --restore &
