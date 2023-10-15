2db6095390c1a48cf19ed7158002ba1c99ef4fbe
Colby Braden <colby.braden@gmail.com>
2023-10-14 22:43:28 -0500

    updated fish configs

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,4 +1,4 @@
 alias v="nvim"
+#alias sway="sway --unsupported-gpu -D noscanout"
 alias reboot="systemctl reboot"
 alias shutdown="systemctl poweroff"
-alias backports="sudo apt -t bookworm-backports install"
