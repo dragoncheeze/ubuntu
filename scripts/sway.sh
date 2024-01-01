85d2e798bb13c0a3016c7ae10fb8f946e12a7741
dragoncheeze <75183285+dragoncheeze@users.noreply.github.com>
2023-12-31 20:09:41 -0600

    Update sway.sh

--- scripts/sway.sh
+++ scripts/sway.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 ## Install Sway Stuff
-sudo apt install sway swaybg swaylock swayidle seatd light playerctl pipewire pipewire-pulse wireplumber pulsemixer wl-clipboard neofetch gtk2-engines-murrine mpv mpd foot i3status python3-i3ipc papirus-icon-theme arc-theme rofi fonts-dejavu fonts-font-awesome fonts-liberation2 fonts-noto fonts-roboto lxpolkit libglib2.0-bin xwayland thunar thunar-archive-plugin file-roller thunar-media-tags-plugin thunar-volman tumbler tumbler-plugins-extra cups galculator build-essential gamemode imv -y
+sudo apt install sway swaybg swaylock swayidle seatd light playerctl \
+pipewire pipewire-pulse wireplumber pulsemixer wl-clipboard neofetch \
+gtk2-engines-murrine mpv mpd foot i3status python3-i3ipc papirus-icon-theme \
+arc-theme rofi fonts-dejavu fonts-font-awesome fonts-liberation2 \
+fonts-noto fonts-roboto lxpolkit libglib2.0-bin xwayland thunar thunar-archive-plugin \
+file-roller thunar-media-tags-plugin thunar-volman tumbler tumbler-plugins-extra \
+cups galculator build-essential gamemode imv -y
