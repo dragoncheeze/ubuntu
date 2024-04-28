bd41216591667502d819b88171938f9ba2448f1b
Colby <colby.braden@gmail.com>
2024-04-28 12:41:18 -0500

    updated default install script

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -4,7 +4,7 @@ sudo apt install xserver-xorg xorg pipewire lxpolkit \
     rsync gtk2-engines-murrine adwaita-icon-theme-full \
     fonts-liberation fonts-liberation2 fonts-noto fonts-roboto \
     fonts-dejavu fonts-font-awesome fonts-ubuntu  \
-    libxkbcommon-dev picom lxappearance conky \
+    libxkbcommon-dev picom lxappearance \
     pavucontrol pulsemixer materia-gtk-theme papirus-icon-theme \
     pcmanfm xarchiver rofi libxft-dev libx11-dev libxinerama-dev \
     galculator geany dunst libnotify-bin suckless-tools build-essential \
