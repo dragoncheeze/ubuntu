d93d8735e570ec4d8e20b6de2db50a198cc660a2
Colby <colby.braden@gmail.com>
2025-08-18 08:16:31 -0500

    updated configs

--- scripts/sway.sh
+++ scripts/sway.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo dpkg --add-architecture i386 && sudo apt update &&
-sudo apt install sway swaybg swaylock swayidle curl \
-    python3-i3ipc seatd dunst light fastfetch playerctl \
-    i3status pipewire-audio nwg-look fwupd libfuse2t64 \
+sudo apt install sway swaybg cargo swaylock swayidle curl \
+    python3-i3ipc pkg-config seatd dunst light fastfetch playerctl \
+    i3status libssl-dev pipewire-audio nwg-look fwupd libfuse2t64 \
     wl-clipboard autotiling gtk2-engines-murrine mpv mpd foot \
     papirus-icon-theme arc-theme fuzzel fonts-dejavu fonts-font-awesome \
     fonts-liberation2 fonts-noto fonts-roboto lxpolkit libglib2.0-bin xwayland \
