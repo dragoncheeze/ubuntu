43fffced20dd8830451efeac609d8f21664a0678
Colby <colby.braden@gmail.com>
2025-08-27 06:25:21 -0500

    updated fish

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,9 +1,5 @@
+fish_add_path ~/.cargo/bin/
 alias v="nvim"
 #alias sway="sway --unsupported-gpu"
 alias reboot="systemctl reboot"
 alias shutdown="systemctl poweroff"
-#alias backports="sudo apt -t trixie-backports install"
-#
-#if uwsm check may-start && uwsm select; then
-#	exec uwsm start default
-#end
